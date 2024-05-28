5500e7376574104a6d4de4160e89a3af3135f9b6
Ikkilink <120761986+Ikkilink@users.noreply.github.com>
2024-05-28 08:25:42 -0600

    audio mapa
    audio mapa

--- Assets/Scripts/Travelqp.cs
+++ Assets/Scripts/Travelqp.cs
@@ -6,27 +6,35 @@ using UnityEngine.UI;
 
 public class Travelqp : MonoBehaviour
 {
+    public AudioClip SonidosRandom;
+    private AudioSource source;
     public GameObject PanelMap;
     private bool Hidden;
     private bool OpenVisible;
     private bool Interact;
+    public bool canopenmap;
 
     public void Start()
     {
+        source = GetComponent<AudioSource>();
         PanelMap.SetActive(Hidden);
         OpenVisible = false;
         Interact = false;
+        canopenmap = false;
     }
     private void Update()
     {
         if (Interact)
         {
             if (Input.GetKeyDown(KeyCode.M) && OpenVisible == false)
-            {
+            {   
                 PanelMap.SetActive(!Hidden);
                 Cursor.lockState = CursorLockMode.None;
                 GetComponent<PlyMovement>().enabled = false;
                 OpenVisible = true;
+                source.clip = SonidosRandom;
+                source.Play();
+
             }
             else if (Input.GetKeyDown(KeyCode.M) && OpenVisible == true)
             {
@@ -34,12 +42,14 @@ public class Travelqp : MonoBehaviour
                 GetComponent<PlyMovement>().enabled = true;
                 PanelMap.SetActive(Hidden);
                 OpenVisible = false;
+                source.clip = SonidosRandom;
+                source.Play();
             }
         }
     }
     private void OnTriggerEnter(Collider other)
     {
-        Interact = true;   
+        Interact = true;
     }
     private void OnTriggerExit(Collider other)
     {
